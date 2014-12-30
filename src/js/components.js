6fcb254d4700428d92ecf46782aa727aa6242820
Danny Graham <good.midget@gmail.com>
2014-12-30 13:53:36 -0800

    added some legacy updates that were forgotten

--- src/js/components.js
+++ src/js/components.js
@@ -7,37 +7,66 @@
 			_tools = $table.attr('am-Tools').split(' '),
 			self = this;
 
-		var _select = function(e){
-			var em = $(e.target).parent();
-			var val = (em.attr('am-Selected') == '') ? 'false' : em.attr('am-Selected');
-			em.attr('am-Selected', (val === 'true')? 'false':'true');
-			em.find('td:first-child input[type=checkbox]').prop('checked',(val === 'true')? false:true);
-			
-			if (_tools.indexOf('select-all') > -1) {
-				if (val === 'true') $table.find('thead tr th:first-child input[type=checkbox]').prop('checked',false);
+		if (_tools.indexOf('select-all') > -1 || _tools.indexOf('select-multi') > -1) {
+			var _select = function(e){
+				var em = $(e.target).parent();
+				var val = (em.attr('am-Selected') == '') ? 'false' : em.attr('am-Selected');
+				em.attr('am-Selected', (val === 'true')? 'false':'true');
+				em.find('td:first-child input[type=checkbox]').prop('checked',(val === 'true')? false:true);
+				
+				if (_tools.indexOf('select-all') > -1) {
+					if (val === 'true') $table.find('thead tr th:first-child input[type=checkbox]').prop('checked',false);
+				}
+
+			};
+
+			var _selectAll = function(e){
+				var em = $(e.target);
+				$table.find('tbody tr').attr('am-Selected',em.prop('checked').toString()).
+				find('td:first-child input[type=checkbox]').prop('checked',em.prop('checked'))
+			};
+
+			this.selected = function(){
+				return $table.find('tbody tr[am-Selected=true]');
 			}
 
-		};
 
-		var _selectAll = function(e){
-			var em = $(e.target);
-			$table.find('tbody tr').attr('am-Selected',em.prop('checked').toString()).
-			find('td:first-child input[type=checkbox]').prop('checked',em.prop('checked'))
-		};
-
-		this.selected = function(){
-			return $table.find('tbody tr[am-Selected=true]');
+			if (_tools.indexOf('select-all') > -1) {
+				$table.on('click','tbody tr td',_select);
+				$table.find('thead tr th:first-child input[type=checkbox]').click(_selectAll);
+			}
 		}
 
-		if (_tools.indexOf('select-all') > -1) {
-			$table.on('click','tbody tr td',_select);
-			$table.find('thead tr th:first-child input[type=checkbox]').click(_selectAll);
+		if (_tools.indexOf('drag-sort') > -1) {
+
+			$table.tableDnD({
+				dragHandle: '.dragHandle'
+			});
+			$table.observe('childlist', 'tbody tr', function(){
+				$table.tableDnDUpdate();
+			});
 		}
 
-		// $table[0].DataTable = this;
+		if (_tools.indexOf('edit-row') > -1 || _tools.indexOf('edit-cell') > -1) {
+
+			if (_tools.indexOf('edit-row') > -1) {
+				this.editRow = function(row) {
+					$('td',row).attr('contenteditable', true);
+				};
+			}
+
+			if (_tools.indexOf('edit-cell') > -1) {
+				$table.observe('childlist', 'tbody tr', function(e){
+					if (e.addedNodes.length) $(e.addedNodes[0]).find('td').attr('contenteditable', true);
+				});
+			}
+		}
+
 	};
 
 	$(document).ready(function(){
+		$('[am-DataTable][am-Tools~=drag-sort]').tableDnD();
+
 		$('[am-DataTable]').each(function(index,table){
 			_DataTables.push(new DataTable(table));
 		});
