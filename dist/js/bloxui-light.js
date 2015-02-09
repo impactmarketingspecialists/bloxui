579026c007a3bdfa0ee111f854a74b22fd879837
Danny Graham <good.midget@gmail.com>
2015-02-09 14:17:17 -0800

    datatable is now a jquery plugin kinda

--- dist/js/bloxui-light.js
+++ dist/js/bloxui-light.js
@@ -4889,22 +4889,29 @@ if (!Array.prototype.indexOf) {
 
 	var _DataTables = [];
 
-	var DataTable = function(_table) {
-		var $table = $(_table),
-			_tools = $table.attr('am-Tools').split(' '),
-			self = this;
+	$.fn.DataTable = function(options) {
 
-		if (_tools.indexOf('select-all') > -1 || _tools.indexOf('select-multi') > -1) {
+		var $table = this;
+		var settings = $.extend({
+			tools: [],
+			sortable: false
+		}, options);
+
+		if (this.attr('am-Tools')) {
+			$.merge(settings.tools, this.attr('am-Tools').split());
+		}
+
+		if (settings.tools.indexOf('select-all') > -1 || settings.tools.indexOf('select-multi') > -1) {
+
 			var _select = function(e){
 				var em = (e.target.nodeName == 'INPUT') ? $(e.target).parent().parent() : $(e.target).parent();
 				var val = (em.attr('am-Selected') == '') ? 'false' : em.attr('am-Selected');
 				em.attr('am-Selected', (val === 'true')? 'false':'true');
 				em.find('td:first-child input[type=checkbox]').prop('checked',(val === 'true')? false:true);
 				
-				if (_tools.indexOf('select-all') > -1) {
+				if (settings.tools.indexOf('select-all') > -1) {
 					if (val === 'true') $table.find('thead tr th:first-child input[type=checkbox]').prop('checked',false);
 				}
-
 			};
 
 			var _selectAll = function(e){
@@ -4917,14 +4924,13 @@ if (!Array.prototype.indexOf) {
 				return $table.find('tbody tr[am-Selected=true]');
 			}
 
-
-			if (_tools.indexOf('select-all') > -1) {
+			if (settings.tools.indexOf('select-all') > -1) {
 				$table.on('click','tbody tr td',_select);
 				$table.find('thead tr th:first-child input[type=checkbox]').click(_selectAll);
 			}
 		}
 
-		if (_tools.indexOf('drag-sort') > -1) {
+		if (settings.tools.indexOf('drag-sort') > -1) {
 
 			$table.tableDnD({
 				dragHandle: '.dragHandle'
@@ -4934,29 +4940,25 @@ if (!Array.prototype.indexOf) {
 			});
 		}
 
-		if (_tools.indexOf('edit-row') > -1 || _tools.indexOf('edit-cell') > -1) {
+		if (settings.tools.indexOf('edit-row') > -1 || settings.tools.indexOf('edit-cell') > -1) {
 
-			if (_tools.indexOf('edit-row') > -1) {
+			if (settings.tools.indexOf('edit-row') > -1) {
 				this.editRow = function(row) {
 					$('td:not(.table-control)',row).attr('contenteditable', true);
 				};
 			}
 
-			if (_tools.indexOf('edit-cell') > -1) {
+			if (settings.tools.indexOf('edit-cell') > -1) {
 				$table.observe('childlist', 'tbody tr', function(e){
 					if (e.addedNodes.length) $(e.addedNodes[0]).find('td:not(.table-control)').attr('contenteditable', true);
 				});
 			}
 		}
+		return this;
 	};
-
-	$.fn.DataTable = DataTable;
 
 	$(document).ready(function(){
 		$('[am-DataTable][am-Tools~=drag-sort]').tableDnD();
-
-		$('[am-DataTable]').each(function(index,table){
-			_DataTables.push(new DataTable(table));
-		});
+		$('[am-DataTable]').DataTable();
 	});
 }(jQuery);
